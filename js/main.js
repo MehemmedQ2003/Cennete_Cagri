de5aad5125603443bafd1b487b5338f9893fbc9d
mehemmed qardasov <116949722+MehemmedQ2003@users.noreply.github.com>
2024-11-10 19:09:20 +0400

    All Pages Updated

--- js/main.js
+++ js/main.js
@@ -1,46 +1,20 @@
-// Get the theme icons and body element
-const sunIcon = document.getElementById('sun-icon');
-const moonIcon = document.getElementById('moon-icon');
-const body = document.body;
+// Modal
+const images = document.querySelectorAll('.row img');
+const modal = document.getElementById('imageModal');
+const modalImage = document.getElementById('fullImage');
+const downloadLink = document.getElementById('downloadLink');
 
-// Apply the stored theme on page load
-const storedTheme = localStorage.getItem('theme');
-if (storedTheme) {
-    body.classList.add(storedTheme);
+images.forEach(img => {
+    img.onclick = function () {
+        modal.style.display = 'block';
+        modalImage.src = this.src;
+        downloadLink.href = this.src;
+    };
+});
+
+function closeModal() {
+    modal.style.display = 'none';
 }
-
-const navLink = document.querySelectorAll('.nav-link');
-
-// Event listeners for theme toggle
-sunIcon.addEventListener('click', () => {
-    body.classList.add('light-theme');
-    body.classList.remove('dark-theme');
-    localStorage.setItem('theme', 'light-theme'); // Store the theme
-});
-
-moonIcon.addEventListener('click', () => {
-    body.classList.add('dark-theme');
-    body.classList.remove('light-theme');
-    localStorage.setItem('theme', 'dark-theme'); // Store the theme
-});
-
-
-// Navbar text underline
-document.addEventListener("DOMContentLoaded", function () {
-    const currentPath = window.location.pathname.split('/').pop();
-
-    const navLinks = document.querySelectorAll(".nav-link");
-
-    navLinks.forEach((link) => {
-        const href = link.getAttribute("href");
-
-        if (href === currentPath) {
-            link.classList.add("active");
-        }
-    });
-});
-
-
 
 
 // Footer
